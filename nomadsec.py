0a7d3a5ba81d30fd6f6ed53519783661471e7e51
Frank Mitchell <me@frank-mitchell.com>
2024-07-13 07:09:51 -0500

    Add command-line arguments, make width of names configurable

--- nomadsec.py
+++ nomadsec.py
@@ -9,7 +9,7 @@ from collections.abc import Collection, Sequence
 from dataclasses import dataclass
 from typing import Protocol
 
-MAX_NAME_LENGTH: int = 14
+DEFAULT_MAX_NAME_LENGTH: int = 14
 
 # coordinates are always f"{width:0d}{height:0d}"
 DEFAULT_SECTOR_HEIGHT: int = 10
@@ -26,12 +26,13 @@ class NameSource(Protocol):
         ...
 
 class SimpleNameSource:
-    def __init__(self, jsonsrc: dict):
+    def __init__(self, jsonsrc: dict, length=DEFAULT_MAX_NAME_LENGTH):
         assert "min_syllables" in jsonsrc
         assert "max_syllables" in jsonsrc
         assert "initial" in jsonsrc
         assert "vowels" in jsonsrc
 
+        self._length = length
         self._min = jsonsrc["min_syllables"]
         self._max = jsonsrc["max_syllables"]
         self._vowels = jsonsrc["vowels"]
@@ -58,8 +59,8 @@ class SimpleNameSource:
         name_seq.append(random.choice(self._final))
 
         result = "".join(name_seq).capitalize()
-        if len(result) > MAX_NAME_LENGTH - 1:
-            return result[: MAX_NAME_LENGTH - 1] + "."
+        if len(result) > self._length - 1:
+            return result[:self._length - 1] + "."
         else:
             return result
 
@@ -83,9 +84,6 @@ class StarHex:
     height: int
     name: str
 
-    def __str__(self):
-        return f"{self.name:14s}{self.width:02d}{self.height:02d}"
-
 
 def sector(height: int, width: int, density: int) -> Collection[StarHex]:
     # Generate a number of stars proportional to density
@@ -97,17 +95,41 @@ def sector(height: int, width: int, density: int) -> Collection[StarHex]:
 
 
 def main() -> None:
-    sector_width: int = DEFAULT_SECTOR_WIDTH
-    sector_height: int = DEFAULT_SECTOR_HEIGHT
-    density: int = DEFAULT_DENSITY
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+            description="Generate a sector for the _FTL: Nomad_ RPG")
+    parser.add_argument("namefile",
+                        help="JSON file specifying random name generator",
+                        type=argparse.FileType(mode="r", encoding="ASCII"))
+    parser.add_argument("-x", "--width",
+                        help="number of hexes/parsecs across",
+                        default=DEFAULT_SECTOR_WIDTH,
+                        type=int)
+    parser.add_argument("-y", "--height",
+                        help="number of hexes/parsecs down",
+                        default=DEFAULT_SECTOR_HEIGHT,
+                        type=int)
+    parser.add_argument("-d", "--density", 
+                        help="density of stars (n in 6)",
+                        default=DEFAULT_DENSITY,
+                        type=int,
+                        choices=range(MINIMUM_DENSITY, MAXIMUM_DENSITY))
+    parser.add_argument("-l", "--length", 
+                        help="maximum length of star names",
+                        default=DEFAULT_MAX_NAME_LENGTH,
+                        type=int)
+    args = parser.parse_args()
+
+    # Record parameters
+    rseed = random.getstate()
 
     namesrc: NameSource = None
 
-    with open(sys.argv[1]) as jsonfile:
-        namesrc = NameGenerator(SimpleNameSource(json.load(jsonfile)))
+    with args.namefile as jsonfile:
+        namesrc = NameGenerator(SimpleNameSource(json.load(jsonfile), length=args.length))
 
     # generate a map of unnamed stars
-    stars: Collection[StarHex] = sector(sector_height, sector_width, density)
+    stars: Collection[StarHex] = sector(args.height, args.width, args.density)
 
     for star in stars:
         # generate a name for the star / planet
@@ -117,8 +139,11 @@ def main() -> None:
     # 4. Print out the map in the requested format:
     #    - Traveller format: see <https://travellermap.com/doc/fileformats>
 
+    # print(f"# {rseed=}")
+    print(f"# width={args.width} height={args.height} density={args.density}")
+    print(f"# stars={len(stars)}")
     for s in stars:
-        print(str(s))
+        print(f"{s.name:{args.length}s}{s.width:02d}{s.height:02d}")
 
 
 if __name__ == "__main__":
